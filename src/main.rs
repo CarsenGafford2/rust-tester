8e230920366b40d81cc870d95270a7311ab4869b
CarsenGafford2 <carsengafford2@gmail.com>
2026-03-26 08:42:23 -0600

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let number: i128 = 40;
+    println!("Fibonacci sequence of: {number}");
+    let result = fibonacci(number);
+    println!("The {number}th Fibonacci number is: {result}");
+
+}
+
+fn fibonacci(n: i128) -> i128 {
+    if n < 0 {
+        panic!("Negative numbers are not allowed in Fibonacci sequence");
+    } else if n <= 1 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
