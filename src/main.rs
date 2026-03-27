cb347ef5810185ed4ea0c5b2934e81e9e8ee2014
CarsenGafford2 <carsengafford2@gmail.com>
2026-03-27 10:00:55 -0600

    work

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use std::io;
-fn main() {
-    println!("What is your name: ");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Failed to read line");
-    if name.trim() == "John" {
-        println!("Poo");
-    } else {
-        println!("Hello, {}", name.trim());
+use eframe::egui;
+
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Clicks: 0");
+            if ui.button("Press me!").clicked() {
+                ui.label("Button was pressed!");
+            }
+        });
     }
+}
+
+fn main() -> eframe::Result<()> {
+    let mut options = eframe::NativeOptions::default();
+    options.vsync = true;
+    eframe::run_native(
+        "Button Clicker",
+        options,
+        Box::new(|_cc| Box::new(MyApp)),
+    )
 }
