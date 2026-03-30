0830de8671dde05ffff19ccaafe0ba3a56795de7
CarsenGafford2 <carsengafford2@gmail.com>
2026-03-30 07:49:01 -0600

    work

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use eframe::egui;
 
-struct MyApp;
+struct MyApp {
+    click_count: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self { click_count: 0 }
+    }
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Clicks: 0");
+            ui.label(format!("Clicks: {}", self.click_count));
+
             if ui.button("Press me!").clicked() {
+                self.click_count += 1;
                 ui.label("Button was pressed!");
             }
         });
@@ -14,11 +24,10 @@ impl eframe::App for MyApp {
 }
 
 fn main() -> eframe::Result<()> {
-    let mut options = eframe::NativeOptions::default();
-    options.vsync = true;
+    let options = eframe::NativeOptions::default();
     eframe::run_native(
         "Button Clicker",
         options,
-        Box::new(|_cc| Box::new(MyApp)),
+        Box::new(|_cc| Box::new(MyApp::default())),
     )
 }
